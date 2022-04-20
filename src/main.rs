88f8359aa16ac2e1122a40f239c8d81ee0967fbe
D4n13l3k00 <D4n13l3k00@yandex.ru>
2022-04-20 17:29:03 +0300

    Change resize method and add width scale parameter

--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,50 @@ use image::GenericImageView;
 use crossterm::{
     event::Event,
     execute, queue,
-    style::{Color, Print, ResetColor, SetBackgroundColor},
+    style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor},
 };
 
 fn main() {
-    if std::env::args().count() != 2 {
-        println!("Usage: {} <image_file>", std::env::args().nth(0).unwrap());
-        return;
-    }
-    crossterm::terminal::SetTitle("imagy - in-terminal image viewer");
     let mut stdout = std::io::stdout();
-    execute!(stdout, crossterm::cursor::Hide).unwrap();
+    execute!(
+        stdout,
+        crossterm::terminal::SetTitle("imagy - in-terminal image viewer")
+    )
+    .unwrap();
 
+    if std::env::args().count() < 2 {
+        execute!(
+            stdout,
+            SetForegroundColor(Color::Green),
+            Print("Usage: "),
+            SetForegroundColor(Color::Yellow),
+            Print("imagy <image> [width scale [2.5]]\n"),
+            ResetColor
+        )
+        .unwrap();
+        return;
+    };
     let image_file = std::env::args().nth(1).unwrap();
+    if !std::path::Path::new(&image_file).exists() {
+        execute!(
+            stdout,
+            SetForegroundColor(Color::Red),
+            Print("Error: "),
+            SetForegroundColor(Color::Yellow),
+            Print("image file does not exist\n"),
+            ResetColor
+        )
+        .unwrap();
+        return;
+    };
 
+    let width_scale = match std::env::args().nth(2) {
+        Some(arg) => arg.parse::<f64>().unwrap_or(2.5),
+        None => 2.5,
+    };
     let img = image::open(&image_file).unwrap();
 
+    execute!(stdout, crossterm::cursor::Hide).unwrap();
     let (mut terminal_width, mut terminal_height) = (0, 0);
     let (img_width, img_height) = img.dimensions();
     loop {
@@ -30,13 +58,14 @@ fn main() {
             terminal_height = t_height;
 
             let mut printing_img = img.clone();
-            if (terminal_width as u32) < img_width || (terminal_height as u32) < img_height {
-                printing_img = printing_img.resize(
-                    terminal_width as u32,
-                    terminal_height as u32,
-                    image::imageops::FilterType::Nearest,
-                );
-            }
+
+            printing_img = printing_img.resize_exact(
+                (img_width as f64 * width_scale) as u32,
+                img_height as u32,
+                image::imageops::FilterType::Nearest,
+            );
+
+            printing_img = printing_img.thumbnail(terminal_width as u32, terminal_height as u32);
 
             queue!(
                 stdout,
@@ -49,26 +78,24 @@ fn main() {
                 let r = pixel.0[0];
                 let g = pixel.0[1];
                 let b = pixel.0[2];
-
-                if x == 0 {
-                    execute!(stdout, crossterm::cursor::MoveTo(0, y as u16)).unwrap();
-                }
                 queue!(
                     stdout,
+                    crossterm::cursor::MoveTo(x as u16, y as u16),
                     SetBackgroundColor(Color::Rgb {
                         r: r as u8,
                         g: g as u8,
                         b: b as u8,
                     }),
-                    Print("  "),
+                    Print(" "),
                     ResetColor
                 )
                 .unwrap();
             }
         }
-        // flush the terminal via crossterm
+
         stdout.flush().unwrap();
-        if crossterm::event::poll(Duration::from_millis(10)).unwrap() {
+
+        if crossterm::event::poll(Duration::from_millis(100)).unwrap() {
             if let Event::Key(event) = crossterm::event::read().unwrap() {
                 if event.code == crossterm::event::KeyCode::Char('q')
                     || event.code == crossterm::event::KeyCode::Esc
@@ -92,11 +119,11 @@ fn main() {
             }
         }
     }
-    // clear terminal
     execute!(
         stdout,
         crossterm::terminal::Clear(crossterm::terminal::ClearType::All),
-        crossterm::cursor::MoveTo(0, 0)
+        crossterm::cursor::MoveTo(0, 0),
+        crossterm::cursor::Show
     )
     .unwrap();
 }
